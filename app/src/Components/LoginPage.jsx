f450d97d2fd39c5b867280f044075b41b3821e15
WonBeomChoi <cwb2310@gmail.com>
2023-10-06 14:42:00 +0900

    메인에 로그인페이지 추가 #1

--- app/src/Components/LoginPage.jsx
+++ app/src/Components/LoginPage.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react"
+
 function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async (email, password)=>{
+        const baseUrl = "https://api.mandarin.weniv.co.kr";
+        const reqPath = "/user/login";
+        const reqUrl = baseUrl+reqPath
+        
+        const loginData = {
+            "user":{
+                "email":email,
+                "password":password
+            }
+        };
+        // 로그인해서 token꺼내기~!
+        const res = await fetch(reqUrl,{
+            method:"POST",
+            headers:{
+                "Content-type":"application/json"
+            },
+            body:JSON.stringify(loginData)
+        });
+        const json = await res.json();
+        console.log(json);
+        const token = json.user.token;
+        console.log(token);
+        // 로컬스토리지에 토큰 저장하기.
+        localStorage.setItem("token",token);
+    }
+
+    const inputEmail = (e)=>{
+        setEmail(e.target.value);
+    }
+    const inputPassword = (e)=>{
+        setPassword(e.target.value);
+    }
+    const submitLogin = (e)=>{
+        e.preventDefault();
+        login(email, password)
+    }
     return(
         <>
             <h1>로그인</h1>
             <section>
                 <h2>이메일, 비밀번호 입력하는곳</h2>
-                <form>
-                    <input type="text" placeholder="이메일입력" />
-                    <input type="text" placeholder="비밀번호입력"/>
+                <form onSubmit={submitLogin}>
+                    <input type="text" placeholder="이메일입력" onChange={inputEmail} value={email}/>
+                    <input type="text" placeholder="비밀번호입력" onChange={inputPassword} value={password}/>
                     <button>로그인</button>
                 </form>
             </section>
