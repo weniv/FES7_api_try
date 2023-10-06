98a53339c9f2e16111fd4af7af6b65099234c201
WonBeomChoi <cwb2310@gmail.com>
2023-10-06 16:51:30 +0900

    중간커밋

--- app/src/Components/JoinPage.jsx
+++ app/src/Components/JoinPage.jsx
@@ -15,9 +15,19 @@ const JoinPage = ()=>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [accountname, setAccountname] = useState("");
+    const [imgSrc, setImgSrc] = useState("https://api.mandarin.weniv.co.kr/Ellipse.png")
 
-    const join = (joinData)=>{
-        console.log(joinData)
+    const join = async (joinData)=>{
+        const reqUrl = "https://api.mandarin.weniv.co.kr/user/";
+        const res = await fetch(reqUrl,{
+            method:"POST",
+            headers:{
+                "Content-type" : "application/json"
+            },
+            body:JSON.stringify(joinData)
+        });
+        const json = await res.json();
+        console.log(json);
     }
     
     const inputUsername = (e)=>{
@@ -32,17 +42,37 @@ const JoinPage = ()=>{
     const inputAccountname = (e)=>{
         setAccountname(e.target.value);
     }
+    const uploadImage = async (imageFile)=>{
+        const reqUrl = "https://api.mandarin.weniv.co.kr/image/uploadfile";
+        // 폼데이터 만들기
+        const form = new FormData();
+        // 폼데이터에 값 추가하기
+        // 폼데이터.append("키","값")
+        form.append("image", imageFile);
+        // 폼바디에 넣어서 요청하기
+        const res = await fetch(reqUrl,{
+            method:"POST",
+            
+        })
 
+    }
+    const handleChangeImage = (e)=>{
+        // 파일 가져오기
+        const imageFile = e.target.files[0];
+        uploadImage(imageFile);        
+    }
     const submitJoin = ()=>{
         const joinData = {
-            username:username,
-            email:email,
-            password:password,
-            accountname:accountname
+            user:{
+                username:username,
+                email:email,
+                password:password,
+                accountname:accountname
+            }
         }
         join(joinData);
     }
-
+    
     return(
         <>
         <section >
@@ -61,7 +91,7 @@ const JoinPage = ()=>{
             <h2 >프로필 설정</h2>
             <p>나중에 언제든지 변경할 수 있습니다.</p>
             <label htmlFor="profileImg">
-                <img src="https://api.mandarin.weniv.co.kr/Ellipse.png" alt="" id="imagePre"/>
+                <img src={imgSrc} alt="" id="imagePre"/>
             </label>
             <input type="file" id="profileImg" name="image" accept="image/*" />
             <div >
